6491224d888fa3019fdf0d23e047bcaf16b971cc
chandan-devs <chandangupta.devs@gmail.com>
2024-02-20 20:02:29 +0530

    Modify src/index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,31 @@
 // require('dotenv').config({path: './env'})
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants.js"
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js"
 
-// import dotenv from "dotenv";
-// import connectDB from "./db/index.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
 
-// dotenv.config({
-//     path: './env'
-// })
+// import express from "express";
+// const app = express()
 
-// connectDB()
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("ERROR: ", error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT} `)
+//         })
+//     }catch (error){
+//         console.log("ERROR: ", error)
+//         throw err
+//     }
+// })()
 
+dotenv.config({
+    path:'./env'
+})
 
-
-
-
-
-
-
-
-import express from "express";
-const app = express()
-
-(async()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error)=>{
-            console.log("ERROR: ", error);
-            throw error
-        })
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT} `)
-        })
-    }catch (error){
-        console.log("ERROR: ", error)
-        throw err
-    }
-})()
+connectDB()
